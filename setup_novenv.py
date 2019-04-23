1d95a4ba2d62eb5a0670e664d862ba1d61d40c9d
eric <ericverhoeff91@hotmail.com>
2019-04-23 14:06:42 +0200

    Create setup_novenv.py
    setup_file for cx_freeze

---
+++ setup_novenv.py
@@ -0,0 +1,39 @@
+import os
+from cx_Freeze import setup, Executable
+import sys
+import distutils
+import opcode
+import os
+from pyproj import datadir
+
+os.environ['TCL_LIBRARY'] = r'C:\Users\VanBoven\AppData\Local\Programs\Python\Python36\tcl\tcl8.6'
+os.environ['TK_LIBRARY'] = r'C:\Users\VanBoven\AppData\Local\Programs\Python\Python36\tcl\tk8.6'
+
+#distutils_path = os.path.join(os.path.dirname(opcode.__file__), 'distutils')
+
+build_exe_options = {"packages":['pandas','shapely','rasterio','fiona','geopandas','numpy','os','utm','tabulate','geopandas','pyproj'],
+                     "include_files":['tcl86t.dll', 'tk86t.dll'],
+                     "includes": ["tkinter"],
+                     }
+
+base = None
+if sys.platform == 'win32':
+    base = "Win32GUI"
+if sys.platform == 'win64':
+    base = "Win64GUI"
+
+#executables = [
+#    Executable('Inversion2QGIS.py.py', base=base, icon = 'logo.ico')
+#] Also add logo.ico to include_files
+
+executables = [
+    Executable('Inversion2QGIS.py', base=base)
+]
+
+    
+setup(name='Inversion2QGIS',
+      version = '1.0',
+      description = 'Tool for converting EM_Tomo output 2 files compatible with qgis',
+      author = "Verhoeff, Vermeer, 2019",
+      options = {"build_exe": build_exe_options},
+      executables = executables)
